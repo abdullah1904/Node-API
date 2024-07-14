5b59c983547c84375f85bf1c0530fda202b305d3
Muhammad Abdullah Zahid <119707334+abdullah1904@users.noreply.github.com>
2024-07-15 02:08:55 +0500

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-const PORT = 8080;
-// const connectionString = "mongodb://localhost:27017/NodeAPI";
-const connectionString = "mongodb://root:abdullah1904@ac-dw1v5p1-shard-00-00.xzbzrr6.mongodb.net:27017,ac-dw1v5p1-shard-00-01.xzbzrr6.mongodb.net:27017,ac-dw1v5p1-shard-00-02.xzbzrr6.mongodb.net:27017/NodeAPI?ssl=true&replicaSet=atlas-e8vsxo-shard-0&authSource=admin&retryWrites=true&w=majority&appName=BackendDB&";
-
-module.exports = {PORT, connectionString};
